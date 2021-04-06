fcef7fd465315109ab6e06c519da1d5fd8257720
vla5924 <vla5924@gmail.com>
2021-04-06 20:29:22 +0300

    Renamed AdditionZero sample

--- 01_peephole_optimization/cpp/AdditionZeroSample.cpp
+++ 01_peephole_optimization/cpp/AdditionZeroSample.cpp
@@ -17,15 +17,15 @@ int main() {
     for (std::uint64_t i = 0; i < 150'000'000; i++) {
         const auto start = std::chrono::high_resolution_clock::now();
         sum_0 += 0;
-        sum_1 += 1;
-        sum_2 += 2;
-        sum_3 += 3;
-        sum_4 += 4;
-        sum_5 += 5;
-        sum_6 += 6;
-        sum_7 += 7;
-        sum_8 += 8;
-        sum_9 += 9;
+        sum_1 += 0;
+        sum_2 += 0;
+        sum_3 += 0;
+        sum_4 += 0;
+        sum_5 += 0;
+        sum_6 += 0;
+        sum_7 += 0;
+        sum_8 += 0;
+        sum_9 += 0;
         const auto finish = std::chrono::high_resolution_clock::now();
         time += std::chrono::duration_cast<std::chrono::duration<double>>(finish - start);
     }
