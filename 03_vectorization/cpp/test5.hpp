994a69a7084b360c3e132ed5f8b74b8d19509984
vla5924 <vla5924@gmail.com>
2021-05-13 00:36:30 +0300

    Finally, code style fixes

--- 03_vectorization/cpp/test5.hpp
+++ 03_vectorization/cpp/test5.hpp
@@ -4,28 +4,28 @@
 #include <ctime>
 #include <vector>
 
-#define N 1024*1024
+#define N 1024 * 1024
 
-struct testFunc{
+struct testFunc {
     testFunc() : a(N), b(N), r(N) {
-        srand (time(NULL));
+        srand(time(NULL));
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Rpass"
 #pragma clang diagnostic ignored "-Rpass-missed"
 #pragma clang diagnostic ignored "-Rpass-analysis"
-        for (int i =0; i < N; i++) {
-           a[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-           b[i] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+        for (int i = 0; i < N; i++) {
+            a[i] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+            b[i] = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
         }
 #pragma clang diagnostic pop
     }
 
-    __attribute__((noinline))
-	void run(){
-		for (int i=0; i < N; i++) {
+    __attribute__((noinline)) void run() {
+        for (int i = 0; i < N; i++) {
             r[i] = a[i] + b[i];
         }
-	}
-private:
-	std::vector<float> a, b, r;
+    }
+
+  private:
+    std::vector<float> a, b, r;
 };
